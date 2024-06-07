3dc6057c1f2d88181c417f32d48131f3b9674e0e
lucasdasilvabezerra <lucas.svbezerra@gmail.com>
2024-06-07 16:22:22 -0300

    Btns

--- js/scripts.js
+++ js/scripts.js
@@ -1,76 +1,54 @@
 // Seleção
-tableTbody = document.querySelector("table tbody")
-
-
-
-
+tableTbody = document.querySelector("table tbody");
 
 // Funções
-addClient = (client) =>{
-    const clientTr = document.createElement("tr")
+addClient = (client) => {
+  const clientTr = document.createElement("tr");
 
-    const clientName = document.createElement("td")
-    clientName.innerText = client.name
-    clientTr.appendChild(clientName)
-    
+  const clientName = document.createElement("td");
+  clientName.innerText = client.name;
+  clientTr.appendChild(clientName);
 
-    const clientEmail = document.createElement("td")
-    clientEmail.innerText = client.email
-    clientTr.appendChild(clientEmail)
-    
+  const clientEmail = document.createElement("td");
+  clientEmail.innerText = client.email;
+  clientTr.appendChild(clientEmail);
 
-    const clientTel = document.createElement("td")
-    clientTel.innerText = client.tel
-    clientTr.appendChild(clientTel)
-    
+  const clientTel = document.createElement("td");
+  clientTel.innerText = client.tel;
+  clientTr.appendChild(clientTel);
 
-    const clientCity = document.createElement("td")
-    clientCity.innerText = client.city
-    clientTr.appendChild(clientCity)
-    
+  const clientCity = document.createElement("td");
+  clientCity.innerText = client.city;
+  clientTr.appendChild(clientCity);
 
+  // Botões de Editar e Excluir
+  const actionBtnsTd = document.createElement("td");
 
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit-btn");
+  editBtn.innerText = "Editar";
 
-    // Botões de Editar e Excluir
-    const actionBtnsTd = document.createElement("td") 
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete-btn");
+  deleteBtn.innerText = "Excluir";
 
-    const editBtn = document.createElement("button")
-    editBtn.classList.add("edit-btn")
-    editBtn.innerText = "Editar"
+  const actionBtnsContainer = document.createElement("div");
+  actionBtnsContainer.classList.add("action-btns");
+  actionBtnsContainer.appendChild(editBtn);
+  actionBtnsContainer.appendChild(deleteBtn);
 
-
-    const deleteBtn = document.createElement("button")
-    deleteBtn.classList.add("delete-btn")
-    deleteBtn.innerText = "Excluir"
-
-
-    const actionBtnsContainer = document.createElement("div")
-    actionBtnsContainer.classList.add("action-btns")
-    actionBtnsContainer.appendChild(editBtn)
-    actionBtnsContainer.appendChild(deleteBtn)
-
-    actionBtnsTd.appendChild(actionBtnsContainer)
-    clientTr.appendChild(actionBtnsTd)
-    tableTbody.appendChild(clientTr)
-
-
-}
-
-
-
+  actionBtnsTd.appendChild(actionBtnsContainer);
+  clientTr.appendChild(actionBtnsTd);
+  tableTbody.appendChild(clientTr);
+};
 
 angela = {
-    name: "Angela",
-    email: "angela@gmail.com",
-    tel: "(44) 99806-1427",
-    city: "Paranavaí"
-}
+  name: "Angela",
+  email: "angela@gmail.com",
+  tel: "(44) 99806-1427",
+  city: "Paranavaí",
+};
 
 addClient(angela);
-
-
-
-
-
 
 // Eventos
